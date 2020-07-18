fe2ae0e9cd0ebf7d09b47ff1fc6e46db5ce9458c
Nabeil Lemar <nabeillemar@gmail.com>
2020-07-18 21:45:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,6 @@ def turn(board)
   user_input = gets.chomp
   index = input_to_index(user_input)
   if valid_move?(board,index)
-    return true
   move(board,index)
   display_board(board)
   else 
