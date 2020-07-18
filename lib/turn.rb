4c03b203d2c3f31f2a7e57a052cad70d4d9d3ab3
Nabeil Lemar <nabeillemar@gmail.com>
2020-07-18 21:00:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ def turn(board)
   puts "Please enter 1-9:"
   string = gets.chomp
   input_to_index(string)
-  if valid_move?(string)
+  if valid_move?(input_to_index)
     return true
   else
   string = gets.chomp
